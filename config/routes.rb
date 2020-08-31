5f3b2485d74c3d4cd641e4ac00bdbbb4f2899103
kazumasa1111 <kazumasa_machida5298@icloud.com>
2020-08-31 15:40:55 +0900

    toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-    resources :home, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+    resources :home
+    resources :users
 end
